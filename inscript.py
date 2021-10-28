78f0f970bb01c7327a1f78a562e3990172a19681
slythethief <93349335+slythethief@users.noreply.github.com>
2021-10-28 15:00:31 -0500

    adding initial file for repo
    script that will install certain packages/files for a fresh install of ubuntu

---
+++ inscript.py
@@ -0,0 +1,24 @@
+import subprocess
+
+'''
+task to accomblish:
+done - download zsh
+change shell from bash to zsh
+download oh-my-zsh
+change couple things in the .zshrc file
+'''
+
+
+def zsh_install():
+    sudo = 'sudo'
+    package = 'zsh'
+    cmd = 'apt'
+    argument = 'install'
+    
+    sub_proc = subprocess.run([sudo, cmd, argument, package], shell=False)
+    
+    subprocess.run(['chsh', '-s', '$(which zsh)'])
+
+zsh_install()
+
+"""/var/log/apt/history.log"""
